50e23494b752cdd315f711549456589cf50d26b1
gildarivera <gilda.rivera@telus.com>
2022-03-25 11:45:08 -0600

    feat: adding regex

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -3,33 +3,36 @@ import Input from "../Input/input";
 export default function Form(props) {
   return (
     <>
-      <div className="w-full max-w-4xl relative form__container">
+      <div className="w-full max-w-6xl relative form__container">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 className="indent-8 text-lg font-bold text-[#000000]">General data</h2>
           <div className="mb-4 form__multiple__inputs">
-              <Input id="nombres" name="Nombres" placeholder="" />
-              <Input id="apellido1" name="Primer apellido" placeholder="" />
-              <Input id="apellido2" name="Segundo apellido" placeholder=""/>
+              <Input id="nombres" name="Nombres" placeholder="" maxlength="15" regex="^[a-zA-Z\s]*$" errorMessage="Los nombres no pueden contener números" required={true}/>
+              <Input id="apellido1" name="Primer apellido" placeholder=""  maxlength="10"/>
+              <Input id="apellido2" name="Segundo apellido" placeholder="" maxlength="10"/>
           </div>
           <div className="mb-4 form__multiple__inputs">
-              <Input id="fecha" name="Fecha" placeholder="" />
+              <Input id="fecha" name="Fecha" placeholder="" type="date" />
               <Input id="email" name="Email" placeholder="" />
               <Input id="direccion" name="Dirección" placeholder=""/>
           </div>
-
+          <h2 className="indent-8 text-lg font-bold text-[#000000]">Información de numero</h2>
           <div className="mb-4 form__multiple__inputs">
               <Input id="codigoMarcado" name="Codigo de marcado" placeholder="" />
               <Input id="numeroTel" name="Número de telefono" placeholder="" />
               <Input id="numeroCasa" name="Número de telefono de casa" placeholder=""/>
           </div>
+          <h2 className="indent-8 text-lg font-bold text-[#000000]">Información de contacto </h2>
           <div className="mb-4 form__multiple__inputs">
               <Input id="numeroIdentificacion" name="Número de identificación personal" placeholder="" />
               <Input id="numeroPasaporte" name="Número de pasaporte" placeholder="" />
               <Input id="datosEmergencia" name="Datos de emergencia" placeholder=""/>
           </div>
+          <h2 className="indent-8 text-lg font-bold text-[#000000]">Información de pago </h2>
           <div className="mb-4 form__multiple__inputs">
-          <Input id="numeroTarjeta" name="Número de tarjeta de credito" placeholder=""/>
-              <Input id="fechaVencimiento" name="Fecha de vencimiento" placeholder="" />
-              <Input id="CVC" name="CVC" placeholder="" />
+          <Input id="numeroTarjeta" name="Número de tarjeta de credito" placeholder="" type="password"/>
+              <Input id="fechaVencimiento" name="Fecha de vencimiento" placeholder="" type="date" />
+              <Input id="CVC" name="CVC" placeholder="" type="password"/>
 
           </div>
 
--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,17 +1,39 @@
-export default function Input(props){
-return <div>
-<label
-  className="block text-gray-700 text-sm font-bold mb-2"
-  htmlFor={props.id}
->
-{props.name}
-</label>
+import { useEffect, useState } from "react";
 
-<input
-  className="shadow  border rounded w-full py-2 px-3  focus:outline-none focus:shadow-outline"
-  id={props.id}
-  type="text"
-  placeholder={props.placeholder}
-/>
-</div>
+export default function Input(props) {
+  const [valid, setValid] = useState(true);
+  const [value, setValue] = useState();
+  useEffect(() => {}, [valid]);
+  const handleChange = (e) => {
+    if (props.regex) {
+       console.log(e.target)
+      var regex = new RegExp(props.regex);
+      if (regex.test(e.target.value)) {
+        setValid(true);
+      } else {
+        setValid(false);
+      }
+    }
+  };
+  return (
+    <div>
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor={props.id}
+      >
+        {props.name}
+      </label>
+
+      <input
+        className="shadow  border rounded w-full py-2 px-3  focus:outline-none focus:shadow-outline"
+        id={props.id}
+        placeholder={props.placeholder}
+        type={props.type ? props.type : "text"}
+        maxLength={props.maxlength}
+        onChange={handleChange}
+        required={props.required}
+      />
+      {valid ? <></> : <p className="text-red-500 text-xs italic">{props.errorMessage}</p>}
+    </div>
+  );
 }
